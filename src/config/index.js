add535d3b1eace7dddff3fa680c5b915753090e9
akshayubale1 <52930002+akshayubale1@users.noreply.github.com>
2024-08-26 03:06:17 +0530

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,18 +1,11 @@
 const db_connection =  () => {
     const conn = {
-        server: process.env.DB_HOST,
-        database: process.env.DB_NAME,
-        options: { encrypt:true }
-    }
-    if (process.env.NODE_ENV && process.env.NODE_ENV == 'development') {
-        conn['user'] = process.env.DB_USER
-        conn['password'] = process.env.DB_PW
-    } else {
-        conn['authentication'] = {
-            type: 'azure-active-directory-msi-app-service'
-        }
-    }
-
+        server: process.env.server,
+        database: process.env.database,
+        options: { encrypt:true, trustServerCertificate: true },
+        user: process.env.username,
+        password: process.env.password
+}
     return conn;
 }
 
